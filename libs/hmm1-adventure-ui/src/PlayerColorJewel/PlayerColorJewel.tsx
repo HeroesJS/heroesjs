7453c810b3e0e36244e0f26fdc6ca10391334dd1
Tomasz Kryśkiewicz <contact@tkryskiewicz.com>
2025-02-09 13:10:42 +0100

    translate player color jewel

--- libs/hmm1-adventure-ui/src/PlayerColorJewel/PlayerColorJewel.tsx
+++ libs/hmm1-adventure-ui/src/PlayerColorJewel/PlayerColorJewel.tsx
@@ -1,36 +1,34 @@
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
 import { PositionedComponent, type PositionProps } from '@heroesjs/hmm1-base-ui';
-import { PlayerColor, playerColors } from '@heroesjs/hmm1-core';
+import { type PlayerColor, playerColors } from '@heroesjs/hmm1-core';
 
 import { gems } from './assets';
-
-const labels: Record<PlayerColor, string> = {
-  [PlayerColor.Blue]: 'Blue',
-  [PlayerColor.Green]: 'Green',
-  [PlayerColor.Red]: 'Red',
-  [PlayerColor.Yellow]: 'Yellow',
-};
 
 interface Props extends PositionProps {
   readonly onClick?: () => void;
   readonly value: PlayerColor;
 }
 
-export const PlayerColorJewel = ({ onClick, value, x, y }: Props) => (
-  <Root x={x} y={y}>
-    <span aria-label="Banner Color" role="radiogroup">
-      {playerColors.map((color) => (
-        <span aria-checked={color === value} key={color} role="radio">
-          {labels[color]}
-        </span>
-      ))}
-    </span>
-    <button aria-label="Change Banner Color" onClick={onClick}>
-      <img alt="" src={gems[value]} />
-    </button>
-  </Root>
-);
+export const PlayerColorJewel = ({ onClick, value, x, y }: Props) => {
+  const { t } = useTranslation(['adventure', 'core'], { keyPrefix: 'component.playerColorJewel' });
+
+  return (
+    <Root x={x} y={y}>
+      <span aria-label={t('label')} role="radiogroup">
+        {playerColors.map((color) => (
+          <span aria-checked={color === value} key={color} role="radio">
+            {t(`core:playerColor.${color}`)}
+          </span>
+        ))}
+      </span>
+      <button aria-label={t('changeLabel')} onClick={onClick}>
+        <img alt="" src={gems[value]} />
+      </button>
+    </Root>
+  );
+};
 
 const Root = styled(PositionedComponent)({
   fontSize: 0,
