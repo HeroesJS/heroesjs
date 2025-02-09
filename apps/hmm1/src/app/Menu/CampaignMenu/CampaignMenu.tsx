ab9db307f8ae6a6a55c9856bdbea1558c107ab1c
Tomasz Kryśkiewicz <contact@tkryskiewicz.com>
2025-02-09 22:56:56 +0100

    refactor campaign menu

--- apps/hmm1/src/app/Menu/CampaignMenu/CampaignMenu.tsx
+++ apps/hmm1/src/app/Menu/CampaignMenu/CampaignMenu.tsx
@@ -1,44 +1,11 @@
 import { useTranslation } from 'react-i18next';
 
-import { type ButtonAssets, Menu, MenuButton, type PositionProps } from '@heroesjs/hmm1-base-ui';
+import { Menu, MenuButton, type PositionProps } from '@heroesjs/hmm1-base-ui';
 import { Campaign } from '@heroesjs/hmm1-core';
 
-import cancelDisabled from './assets/cancel/disabled.png';
-import cancelEnabled from './assets/cancel/enabled.png';
-import playLordAlamarDisabled from './assets/play-lord-alamar/disabled.png';
-import playLordAlamarEnabled from './assets/play-lord-alamar/enabled.png';
-import playLordIronfistDisabled from './assets/play-lord-ironfist/disabled.png';
-import playLordIronfistEnabled from './assets/play-lord-ironfist/enabled.png';
-import playLordSlayerDisabled from './assets/play-lord-slayer/disabled.png';
-import playLordSlayerEnabled from './assets/play-lord-slayer/enabled.png';
-import playQueenLamandaDisabled from './assets/play-queen-lamanda/disabled.png';
-import playQueenLamandaEnabled from './assets/play-queen-lamanda/enabled.png';
+import * as assets from './assets';
 
 const campaigns = [Campaign.LordIronfist, Campaign.LordSlayer, Campaign.QueenLamanda, Campaign.LordAlamar] as const;
-
-const assets: Record<Campaign, ButtonAssets> = {
-  [Campaign.LordAlamar]: {
-    disabled: playLordAlamarDisabled,
-    enabled: playLordAlamarEnabled,
-  },
-  [Campaign.LordIronfist]: {
-    disabled: playLordIronfistDisabled,
-    enabled: playLordIronfistEnabled,
-  },
-  [Campaign.LordSlayer]: {
-    disabled: playLordSlayerDisabled,
-    enabled: playLordSlayerEnabled,
-  },
-  [Campaign.QueenLamanda]: {
-    disabled: playQueenLamandaDisabled,
-    enabled: playQueenLamandaEnabled,
-  },
-};
-
-const cancelButtonAssets: ButtonAssets = {
-  disabled: cancelDisabled,
-  enabled: cancelEnabled,
-};
 
 interface Props extends PositionProps {
   readonly onCampaignClick?: (value: Campaign) => void;
@@ -53,7 +20,7 @@ export const CampaignMenu = ({ onCampaignClick, onCancelClick, x, y }: Props) =>
       {campaigns.map((campaign) => (
         <Item key={campaign} onClick={onCampaignClick} value={campaign} />
       ))}
-      <MenuButton assets={cancelButtonAssets} label={t('cancel')} onClick={onCancelClick} />
+      <MenuButton assets={assets.cancelButton} label={t('cancel')} onClick={onCancelClick} />
     </Menu>
   );
 };
@@ -68,5 +35,5 @@ const Item = ({ onClick, value }: ItemProps) => {
 
   const handleClick = () => onClick?.(value);
 
-  return <MenuButton assets={assets[value]} label={t(value)} onClick={handleClick} />;
+  return <MenuButton assets={assets.campaignButtons[value]} label={t(value)} onClick={handleClick} />;
 };
---
+++ apps/hmm1/src/app/Menu/CampaignMenu/assets/play-lord-slayer/index.ts
@@ -0,0 +1,9 @@
+import type { ButtonAssets } from '@heroesjs/hmm1-base-ui';
+
+import disabled from './disabled.png';
+import enabled from './enabled.png';
+
+export const playLordSlayerButton: ButtonAssets = {
+  disabled,
+  enabled,
+};
