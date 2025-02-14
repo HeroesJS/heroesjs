d5ca90123efc0e20895616c2627bbd03ae89daf4
Tomasz Kryśkiewicz <contact@tkryskiewicz.com>
2025-02-14 22:36:31 +0100

    add host-guest menu tests

--- apps/hmm1/src/app/Menu/HostGuestMenu/HostGuestMenu.tsx
+++ apps/hmm1/src/app/Menu/HostGuestMenu/HostGuestMenu.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 
-import { Menu, MenuButton, MenuSeparator, type PositionProps } from '@heroesjs/hmm1-base-ui';
+import { Menu, MenuButton, MenuSeparator, Modal, type PositionProps, useModal } from '@heroesjs/hmm1-base-ui';
 
 import * as assets from './assets';
 
@@ -14,21 +14,46 @@ interface Props extends PositionProps {
 export const HostGuestMenu = ({ detailed, onCancelClick, onGuestClick, onHostClick, x, y }: Props) => {
   const { t } = useTranslation('main', { keyPrefix: 'component.hostGuestMenu' });
 
+  const hostInfo = useModal();
+  const guestInfo = useModal();
+  const cancelInfo = useModal();
+
   return (
-    <Menu label={t('title')} x={x} y={y}>
-      <MenuButton
-        assets={detailed ? assets.hostDialsButton : assets.hostButton}
-        label={t(detailed ? 'hostDetail' : 'host')}
-        onClick={onHostClick}
-      />
-      <MenuButton
-        assets={detailed ? assets.guestAnswersButton : assets.guestButton}
-        label={t(detailed ? 'guestDetail' : 'guest')}
-        onClick={onGuestClick}
-      />
-      <MenuSeparator />
-      <MenuSeparator />
-      <MenuButton assets={assets.cancelButton} label={t('cancel')} onClick={onCancelClick} />
-    </Menu>
+    <>
+      <Menu label={t('title')} x={x} y={y}>
+        <MenuButton
+          assets={detailed ? assets.hostDialsButton : assets.hostButton}
+          label={t(detailed ? 'hostDetail' : 'host')}
+          onClick={onHostClick}
+          onRightButtonDown={hostInfo.open}
+          onRightButtonUp={hostInfo.close}
+        />
+        <MenuButton
+          assets={detailed ? assets.guestAnswersButton : assets.guestButton}
+          label={t(detailed ? 'guestDetail' : 'guest')}
+          onClick={onGuestClick}
+          onRightButtonDown={guestInfo.open}
+          onRightButtonUp={guestInfo.close}
+        />
+        <MenuSeparator />
+        <MenuSeparator />
+        <MenuButton
+          assets={assets.cancelButton}
+          label={t('cancel')}
+          onClick={onCancelClick}
+          onRightButtonDown={cancelInfo.open}
+          onRightButtonUp={cancelInfo.close}
+        />
+      </Menu>
+      <Modal open={hostInfo.isOpen} x={177} y={29}>
+        {t(detailed ? 'hostDetailInfo' : 'hostInfo')}
+      </Modal>
+      <Modal open={guestInfo.isOpen} x={177} y={29}>
+        {t(detailed ? 'guestDetailInfo' : 'guestInfo')}
+      </Modal>
+      <Modal open={cancelInfo.isOpen} x={177} y={29}>
+        {t('cancelInfo')}
+      </Modal>
+    </>
   );
 };
