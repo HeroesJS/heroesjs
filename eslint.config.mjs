3a32bc2fe6020dbc597964293e244675c42d5816
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2025-11-17 13:30:20 +0100

    fix unnecessary linting of typings files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,13 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/vite.config.*.timestamp*', '**/vitest.config.*.timestamp*', '**/test-output'],
+    ignores: [
+      '**/dist',
+      '**/out-tsc',
+      '**/vite.config.*.timestamp*',
+      '**/vitest.config.*.timestamp*',
+      '**/test-output',
+    ],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
