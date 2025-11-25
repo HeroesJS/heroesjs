59030c77420edd39379b93a0efc556cd0d84f410
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2025-11-25 13:56:09 +0100

    add position component

---
+++ apps/hmm1/src/app/PositionedComponent/PositionedComponent.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export interface PositionProps {
+  readonly x?: number;
+  readonly y?: number;
+}
+
+export const PositionedComponent = styled('div')<PositionProps>(({ x, y }) => ({
+  left: x,
+  position: x !== undefined && y !== undefined ? 'absolute' : 'unset',
+  top: y,
+}));
