66ea92f8b07239f276fb2a053bf6c9419c877f54
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2025-11-26 13:11:29 +0100

    add button component

---
+++ apps/hmm1/src/app/Button/storyAssets/index.ts
@@ -0,0 +1,9 @@
+import type { ButtonAssets } from '../../Button';
+
+import active from './active.png';
+import pressed from './pressed.png';
+
+export const assets: ButtonAssets = {
+  active,
+  pressed,
+};
