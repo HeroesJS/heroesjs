d35e17ec6e2974c7e59d89d81de3d92f7fe787b5
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2025-12-09 20:10:11 +0100

    extract main screen pom

--- apps/hmm1-e2e/src/mainScreen.test.ts
+++ apps/hmm1-e2e/src/mainScreen.test.ts
@@ -1,66 +1,64 @@
 import { expect, test } from './utils';
 
-test('displays main screen', async ({ page }) => {
-  await page.goto('/');
+test('displays main screen', async ({ mainScreen }) => {
+  await mainScreen.goto();
 
-  await expect(page.getByRole('main', { name: /main screen/i })).toBeVisible();
+  await expect(mainScreen.locator).toBeVisible();
 });
 
 test.describe('main menu', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
+  test.beforeEach(async ({ mainScreen }) => {
+    await mainScreen.goto();
   });
 
-  test('displays main menu', async ({ page }) => {
-    await expect(page.getByRole('menu', { name: /main menu/i })).toBeVisible();
+  test('displays main menu', async ({ mainScreen, page }) => {
+    await expect(mainScreen.menu).toBeVisible();
 
-    await expect(page.getByRole('button', { name: /new game/i })).toBeVisible();
-    await expect(page.getByRole('button', { name: /load game/i })).toBeVisible();
-    await expect(page.getByRole('button', { name: /view high scores/i })).toBeVisible();
-    await expect(page.getByRole('button', { name: /view credits/i })).toBeVisible();
-    await expect(page.getByRole('button', { name: /quit/i })).toBeVisible();
+    await expect(mainScreen.newGameButton).toBeVisible();
+    await expect(mainScreen.loadGameButton).toBeVisible();
+    await expect(mainScreen.viewHighScoresButton).toBeVisible();
+    await expect(mainScreen.viewCreditsButton).toBeVisible();
+    await expect(mainScreen.quitButton).toBeVisible();
 
     await expect(page).toHaveScreenshot('main-menu.png', { maxDiffPixelRatio: 0.05 });
   });
 
-  test('displays new game info', async ({ mouseRightDown, page }) => {
-    await mouseRightDown(page.getByRole('button', { name: /new game/i }));
+  test('displays new game info', async ({ mainScreen, mouseRightDown, page }) => {
+    await mouseRightDown(mainScreen.newGameButton);
 
-    await expect(page.getByRole('dialog', { name: /start a single or multi-player game\./i })).toBeVisible();
+    await expect(mainScreen.newGameInfoModal).toBeVisible();
 
     await expect(page).toHaveScreenshot('main-menu-new-game-info.png', { maxDiffPixelRatio: 0.05 });
   });
 
-  test('displays load game info', async ({ mouseRightDown, page }) => {
-    await mouseRightDown(page.getByRole('button', { name: /load game/i }));
+  test('displays load game info', async ({ mainScreen, mouseRightDown, page }) => {
+    await mouseRightDown(mainScreen.loadGameButton);
 
-    await expect(page.getByRole('dialog', { name: /load a previously saved game\./i })).toBeVisible();
+    await expect(mainScreen.loadGameInfoModal).toBeVisible();
 
     await expect(page).toHaveScreenshot('main-menu-load-game-info.png', { maxDiffPixelRatio: 0.05 });
   });
 
-  test('displays view high scores info', async ({ mouseRightDown, page }) => {
-    await mouseRightDown(page.getByRole('button', { name: /view high scores/i }));
+  test('displays view high scores info', async ({ mainScreen, mouseRightDown, page }) => {
+    await mouseRightDown(mainScreen.viewHighScoresButton);
 
-    await expect(page.getByRole('dialog', { name: /view the high score screen\./i })).toBeVisible();
+    await expect(mainScreen.viewHighScoresInfoModal).toBeVisible();
 
     await expect(page).toHaveScreenshot('main-menu-view-high-scores-info.png', { maxDiffPixelRatio: 0.05 });
   });
 
-  test('displays view credits info', async ({ mouseRightDown, page }) => {
-    await mouseRightDown(page.getByRole('button', { name: /view credits/i }));
+  test('displays view credits info', async ({ mainScreen, mouseRightDown, page }) => {
+    await mouseRightDown(mainScreen.viewCreditsButton);
 
-    await expect(page.getByRole('dialog', { name: /view the credits screen\./i })).toBeVisible();
+    await expect(mainScreen.viewCreditsInfoModal).toBeVisible();
 
     await expect(page).toHaveScreenshot('main-menu-view-credits-info.png', { maxDiffPixelRatio: 0.05 });
   });
 
-  test('displays quit info', async ({ mouseRightDown, page }) => {
-    await mouseRightDown(page.getByRole('button', { name: /quit/i }));
+  test('displays quit info', async ({ mainScreen, mouseRightDown, page }) => {
+    await mouseRightDown(mainScreen.quitButton);
 
-    await expect(
-      page.getByRole('dialog', { name: /quit heroes of might and magic and return to the dos prompt\./i })
-    ).toBeVisible();
+    await expect(mainScreen.quitInfoModal).toBeVisible();
 
     await expect(page).toHaveScreenshot('main-menu-quit-info.png', { maxDiffPixelRatio: 0.05 });
   });
--- apps/hmm1-e2e/src/utils.ts
+++ apps/hmm1-e2e/src/utils.ts
@@ -1,12 +1,16 @@
 import { expect, type Locator, test as testBase } from '@playwright/test';
 
+import { MainScreen } from './mainScreen';
+
 export { expect };
 
 interface Fixtures {
+  readonly mainScreen: MainScreen;
   readonly mouseRightDown: (locator: Locator) => Promise<void>;
 }
 
 export const test = testBase.extend<Fixtures>({
+  mainScreen: async ({ page }, use) => await use(new MainScreen(page)),
   mouseRightDown: async ({ page }, use) =>
     await use(async (locator: Locator) => {
       const button = (await locator.boundingBox())!;
