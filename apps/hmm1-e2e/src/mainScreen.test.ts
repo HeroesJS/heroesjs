9e4e6a080ea32cd667d04b4aa6d951cd2f11beb4
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2025-12-10 11:13:12 +0100

    refactor main screen tests

--- apps/hmm1-e2e/src/mainScreen.test.ts
+++ apps/hmm1-e2e/src/mainScreen.test.ts
@@ -1,65 +1,61 @@
 import { expect, test } from './utils';
 
-test('displays main screen', async ({ mainScreen }) => {
+test.beforeEach(async ({ mainScreen }) => {
   await mainScreen.goto();
+});
 
+test('displays screen', async ({ mainScreen }) => {
   await expect(mainScreen.locator).toBeVisible();
 });
 
-test.describe('main menu', () => {
-  test.beforeEach(async ({ mainScreen }) => {
-    await mainScreen.goto();
-  });
+test('displays menu', async ({ mainScreen, page }) => {
+  await expect(mainScreen.menu).toBeVisible();
 
-  test('displays main menu', async ({ mainScreen, page }) => {
-    await expect(mainScreen.menu).toBeVisible();
+  await expect(mainScreen.newGameButton).toBeVisible();
+  await expect(mainScreen.loadGameButton).toBeVisible();
+  await expect(mainScreen.viewHighScoresButton).toBeVisible();
+  await expect(mainScreen.viewCreditsButton).toBeVisible();
+  await expect(mainScreen.quitButton).toBeVisible();
 
-    await expect(mainScreen.newGameButton).toBeVisible();
-    await expect(mainScreen.loadGameButton).toBeVisible();
-    await expect(mainScreen.viewHighScoresButton).toBeVisible();
-    await expect(mainScreen.viewCreditsButton).toBeVisible();
-    await expect(mainScreen.quitButton).toBeVisible();
+  await expect(page).toHaveScreenshot('screenshot.png', { maxDiffPixelRatio: 0.05 });
+});
 
-    await expect(page).toHaveScreenshot('main-menu.png', { maxDiffPixelRatio: 0.05 });
-  });
+test('displays new game info', async ({ mainScreen, mouseRightDown, page }) => {
+  await mouseRightDown(mainScreen.newGameButton);
 
-  test('displays new game info', async ({ mainScreen, mouseRightDown, page }) => {
-    await mouseRightDown(mainScreen.newGameButton);
+  await expect(mainScreen.newGameInfoModal).toBeVisible();
 
-    await expect(mainScreen.newGameInfoModal).toBeVisible();
+  await expect(page).toHaveScreenshot('new-game-info.png', { maxDiffPixelRatio: 0.05 });
+});
 
-    await expect(page).toHaveScreenshot('main-menu-new-game-info.png', { maxDiffPixelRatio: 0.05 });
-  });
+test('displays load game info', async ({ mainScreen, mouseRightDown, page }) => {
+  await mouseRightDown(mainScreen.loadGameButton);
 
-  test('displays load game info', async ({ mainScreen, mouseRightDown, page }) => {
-    await mouseRightDown(mainScreen.loadGameButton);
+  await expect(mainScreen.loadGameInfoModal).toBeVisible();
 
-    await expect(mainScreen.loadGameInfoModal).toBeVisible();
+  await expect(page).toHaveScreenshot('load-game-info.png', { maxDiffPixelRatio: 0.05 });
+});
 
-    await expect(page).toHaveScreenshot('main-menu-load-game-info.png', { maxDiffPixelRatio: 0.05 });
-  });
+test('displays view high scores info', async ({ mainScreen, mouseRightDown, page }) => {
+  await mouseRightDown(mainScreen.viewHighScoresButton);
 
-  test('displays view high scores info', async ({ mainScreen, mouseRightDown, page }) => {
-    await mouseRightDown(mainScreen.viewHighScoresButton);
+  await expect(mainScreen.viewHighScoresInfoModal).toBeVisible();
 
-    await expect(mainScreen.viewHighScoresInfoModal).toBeVisible();
+  await expect(page).toHaveScreenshot('view-high-scores-info.png', { maxDiffPixelRatio: 0.05 });
+});
 
-    await expect(page).toHaveScreenshot('main-menu-view-high-scores-info.png', { maxDiffPixelRatio: 0.05 });
-  });
+test('displays view credits info', async ({ mainScreen, mouseRightDown, page }) => {
+  await mouseRightDown(mainScreen.viewCreditsButton);
 
-  test('displays view credits info', async ({ mainScreen, mouseRightDown, page }) => {
-    await mouseRightDown(mainScreen.viewCreditsButton);
+  await expect(mainScreen.viewCreditsInfoModal).toBeVisible();
 
-    await expect(mainScreen.viewCreditsInfoModal).toBeVisible();
+  await expect(page).toHaveScreenshot('view-credits-info.png', { maxDiffPixelRatio: 0.05 });
+});
 
-    await expect(page).toHaveScreenshot('main-menu-view-credits-info.png', { maxDiffPixelRatio: 0.05 });
-  });
+test('displays quit info', async ({ mainScreen, mouseRightDown, page }) => {
+  await mouseRightDown(mainScreen.quitButton);
 
-  test('displays quit info', async ({ mainScreen, mouseRightDown, page }) => {
-    await mouseRightDown(mainScreen.quitButton);
+  await expect(mainScreen.quitInfoModal).toBeVisible();
 
-    await expect(mainScreen.quitInfoModal).toBeVisible();
-
-    await expect(page).toHaveScreenshot('main-menu-quit-info.png', { maxDiffPixelRatio: 0.05 });
-  });
+  await expect(page).toHaveScreenshot('quit-info.png', { maxDiffPixelRatio: 0.05 });
 });
