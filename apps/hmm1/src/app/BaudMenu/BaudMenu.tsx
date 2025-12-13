89eac425664a90a12fc1fdb8cdbc51556e12729a
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2025-12-13 16:26:17 +0100

    implement menu

---
+++ apps/hmm1/src/app/BaudMenu/BaudMenu.tsx
@@ -0,0 +1,31 @@
+import { Button, type ButtonAssets } from '../Button';
+import { Menu, MenuItem } from '../Menu';
+import type { PositionProps } from '../PositionedComponent';
+import { baud19200, baud2400, baud38400, baud9600, cancel } from './assets';
+
+interface BaudMenuProps extends PositionProps {
+  readonly onValueClick?: (value: number) => void;
+  readonly onCancelClick?: () => void;
+}
+
+export function BaudMenu({ onCancelClick, onValueClick, x, y }: BaudMenuProps) {
+  const assets: Record<number, ButtonAssets> = {
+    19200: baud19200,
+    2400: baud2400,
+    38400: baud38400,
+    9600: baud9600,
+  };
+
+  return (
+    <Menu label="Baud Menu" x={x} y={y}>
+      {[2400, 9600, 19200, 38400].map((value) => (
+        <MenuItem key={value}>
+          <Button assets={assets[value]} label={`${value} Baud`} onClick={() => onValueClick?.(value)} />
+        </MenuItem>
+      ))}
+      <MenuItem>
+        <Button assets={cancel} label="Cancel" onClick={onCancelClick} />
+      </MenuItem>
+    </Menu>
+  );
+}
---
+++ apps/hmm1/src/app/BaudMenu/index.ts
@@ -0,0 +1,1 @@
+export { BaudMenu } from './BaudMenu';
