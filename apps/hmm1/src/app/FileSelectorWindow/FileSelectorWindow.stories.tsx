6d72a7bd6236df02c50883c6d933f9b6b26c79dd
Tomasz Kryśkiewicz <tkryskiewicz@users.noreply.github.com>
2026-01-15 18:54:31 +0100

    upgrade nx (#24)
    * upgrade nx
    
    * fix lint errors
    
    ---------
    
    Co-authored-by: Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>

--- apps/hmm1/src/app/FileSelectorWindow/FileSelectorWindow.stories.tsx
+++ apps/hmm1/src/app/FileSelectorWindow/FileSelectorWindow.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { MapDifficulty, MapSize } from '../map';
 import { FileSelectorWindow } from './FileSelectorWindow';
