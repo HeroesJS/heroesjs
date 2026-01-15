26add7e24dd351505308bae66406566eafd77d31
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2026-01-15 18:39:44 +0100

    upgrade nx

--- apps/hmm1/eslint.config.mjs
+++ apps/hmm1/eslint.config.mjs
@@ -1,9 +1,11 @@
 import nx from '@nx/eslint-plugin';
+import storybook from 'eslint-plugin-storybook';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
   ...baseConfig,
   ...nx.configs['flat/react'],
+  ...storybook.configs['flat/recommended'],
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     // Override or add rules here
