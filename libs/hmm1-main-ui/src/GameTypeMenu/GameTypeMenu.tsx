d7b586a781ad858a77fb0eefaa97e46f405a6b53
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2026-03-07 14:02:25 +0100

    translate game type menu

--- libs/hmm1-main-ui/src/GameTypeMenu/GameTypeMenu.tsx
+++ libs/hmm1-main-ui/src/GameTypeMenu/GameTypeMenu.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import { Button, Menu, MenuItem, useModal } from '@heroesjs/hmm1-core-ui';
 
 import { campaignGame, cancel, multiPlayerGame, standardGame } from './assets';
@@ -19,46 +21,51 @@ export function GameTypeMenu({
   x,
   y,
 }: GameTypeMenuProps) {
+  const { t } = useTranslation('main', { keyPrefix: 'component.gameTypeMenu' });
+
   const [StandardGameInfoModal, standardGameInfoModal] = useModal();
   const [CampaignGameInfoModal, campaignGameInfoModal] = useModal();
   const [MultiPlayerGameInfoModal, multiPlayerGameInfoModal] = useModal();
   const [CancelInfoModal, cancelInfoModal] = useModal();
 
   return (
-    <Menu label="Game Type Menu" x={x} y={y}>
+    <Menu label={t('label')} x={x} y={y}>
       <MenuItem>
         <Button
           assets={standardGame}
-          label="Standard Game"
+          label={t('standardGame.label')}
           onClick={onStandardGameClick}
           onMouseDown={standardGameInfoModal.onMouseDown}
         />
-        <StandardGameInfoModal>A single player game playing out a single map.</StandardGameInfoModal>
+        <StandardGameInfoModal>{t('standardGame.info')}</StandardGameInfoModal>
       </MenuItem>
       <MenuItem>
         <Button
           assets={campaignGame}
-          label="Campaign Game"
+          label={t('campaignGame.label')}
           onClick={onCampaignGameClick}
           onMouseDown={campaignGameInfoModal.onMouseDown}
         />
-        <CampaignGameInfoModal>A single player game playing through a series of maps.</CampaignGameInfoModal>
+        <CampaignGameInfoModal>{t('campaignGame.info')}</CampaignGameInfoModal>
       </MenuItem>
       <MenuItem>
         <Button
           assets={multiPlayerGame}
-          label="Multi-Player Game"
+          label={t('multiPlayerGame.label')}
           onClick={onMultiPlayerGameClick}
           onMouseDown={multiPlayerGameInfoModal.onMouseDown}
         />
-        <MultiPlayerGameInfoModal size={1}>
-          A multi-player game, with several human players competing against each other on a single map.
-        </MultiPlayerGameInfoModal>
+        <MultiPlayerGameInfoModal size={1}>{t('multiPlayerGame.info')}</MultiPlayerGameInfoModal>
       </MenuItem>
       <MenuItem />
       <MenuItem>
-        <Button assets={cancel} label="Cancel" onClick={onCancelClick} onMouseDown={cancelInfoModal.onMouseDown} />
-        <CancelInfoModal>Cancel back to the main menu.</CancelInfoModal>
+        <Button
+          assets={cancel}
+          label={t('cancel.label')}
+          onClick={onCancelClick}
+          onMouseDown={cancelInfoModal.onMouseDown}
+        />
+        <CancelInfoModal>{t('cancel.info')}</CancelInfoModal>
       </MenuItem>
     </Menu>
   );
