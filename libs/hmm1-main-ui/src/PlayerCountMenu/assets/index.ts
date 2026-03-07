5876760eb260625a887755942312228c1c95a0e5
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2026-03-07 18:22:06 +0100

    translate player count menu

--- libs/hmm1-main-ui/src/PlayerCountMenu/assets/index.ts
+++ libs/hmm1-main-ui/src/PlayerCountMenu/assets/index.ts
@@ -1,4 +1,13 @@
-export { assets as twoPlayers } from './2-players';
-export { assets as threePlayers } from './3-players';
-export { assets as fourPlayers } from './4-players';
+import type { ButtonAssets } from '@heroesjs/hmm1-core-ui';
+
+import { assets as twoPlayers } from './2-players';
+import { assets as threePlayers } from './3-players';
+import { assets as fourPlayers } from './4-players';
+
+export const countAssets: Readonly<Record<number, ButtonAssets>> = {
+  2: twoPlayers,
+  3: threePlayers,
+  4: fourPlayers,
+};
+
 export { assets as cancel } from './cancel';
