de71f55cb70b296b0eec9abe8b01b3de8d286c54
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2026-03-07 15:19:47 +0100

    translate network game menu

--- libs/hmm1-main-ui/src/NetworkGameMenu/NetworkGameMenu.tsx
+++ libs/hmm1-main-ui/src/NetworkGameMenu/NetworkGameMenu.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import { Button, Menu, MenuItem, useModal } from '@heroesjs/hmm1-core-ui';
 
 import { cancel, guest, host } from './assets';
@@ -11,30 +13,37 @@ interface NetworkGameMenuProps {
 }
 
 export function NetworkGameMenu({ onCancelClick, onGuestClick, onHostClick, x, y }: NetworkGameMenuProps) {
+  const { t } = useTranslation('main', { keyPrefix: 'component.networkGameMenu' });
+
   const [HostInfoModal, hostInfoModal] = useModal();
   const [GuestInfoModal, guestInfoModal] = useModal();
   const [CancelInfoModal, cancelInfoModal] = useModal();
 
   return (
-    <Menu label="Network Game Menu" x={x} y={y}>
+    <Menu label={t('label')} x={x} y={y}>
       <MenuItem>
-        <Button assets={host} label="Host" onClick={onHostClick} onMouseDown={hostInfoModal.onMouseDown} />
-        <HostInfoModal>
-          The host sets up the game options.{'  '}There can only be one host per network game.
-        </HostInfoModal>
+        <Button assets={host} label={t('host.label')} onClick={onHostClick} onMouseDown={hostInfoModal.onMouseDown} />
+        <HostInfoModal>{t('host.info')}</HostInfoModal>
       </MenuItem>
       <MenuItem>
-        <Button assets={guest} label="Guest" onClick={onGuestClick} onMouseDown={guestInfoModal.onMouseDown} />
-        <GuestInfoModal size={1}>
-          The guest waits for the host to set up the game, then is automatically added in.{'  '}There can only be one
-          guest per network game.
-        </GuestInfoModal>
+        <Button
+          assets={guest}
+          label={t('guest.label')}
+          onClick={onGuestClick}
+          onMouseDown={guestInfoModal.onMouseDown}
+        />
+        <GuestInfoModal size={1}>{t('guest.info')}</GuestInfoModal>
       </MenuItem>
       <MenuItem />
       <MenuItem />
       <MenuItem>
-        <Button assets={cancel} label="Cancel" onClick={onCancelClick} onMouseDown={cancelInfoModal.onMouseDown} />
-        <CancelInfoModal>Cancel back to the main menu.</CancelInfoModal>
+        <Button
+          assets={cancel}
+          label={t('cancel.label')}
+          onClick={onCancelClick}
+          onMouseDown={cancelInfoModal.onMouseDown}
+        />
+        <CancelInfoModal>{t('cancel.info')}</CancelInfoModal>
       </MenuItem>
     </Menu>
   );
