2dde734872c340a4ccb78cdea352a03f8f05f5b3
Tomasz Kryśkiewicz <19226713+tkryskiewicz@users.noreply.github.com>
2026-03-07 09:59:21 +0100

    translate com port menu

--- libs/hmm1-main-ui/src/ComPortMenu/assets/index.ts
+++ libs/hmm1-main-ui/src/ComPortMenu/assets/index.ts
@@ -1,5 +1,15 @@
+import type { ButtonAssets } from '@heroesjs/hmm1-core-ui';
+
+import { assets as com1 } from './com-1';
+import { assets as com2 } from './com-2';
+import { assets as com3 } from './com-3';
+import { assets as com4 } from './com-4';
+
+export const portAssets: Readonly<Record<number, ButtonAssets>> = {
+  1: com1,
+  2: com2,
+  3: com3,
+  4: com4,
+};
+
 export { assets as cancel } from './cancel';
-export { assets as com1 } from './com-1';
-export { assets as com2 } from './com-2';
-export { assets as com3 } from './com-3';
-export { assets as com4 } from './com-4';
